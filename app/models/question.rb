f7f8be03205f1dbe2e2e985170872cea413f15fa
rakeLL <satbirdd@gmail.com>
2014-12-23 22:44:43 +0800

    tags

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :asker, class_name: 'User', foreign_key: :user_id
+
+  has_many :question_tags
+  has_many :tags, through: :question_tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141223134643) do
+ActiveRecord::Schema.define(version: 20141223143803) do
+
+  create_table "question_tags", force: true do |t|
+    t.integer  "question_id"
+    t.integer  "tag_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "question_tags", ["question_id"], name: "index_question_tags_on_question_id"
+  add_index "question_tags", ["tag_id"], name: "index_question_tags_on_tag_id"
 
   create_table "questions", force: true do |t|
     t.string   "title"
@@ -22,6 +32,12 @@ ActiveRecord::Schema.define(version: 20141223134643) do
   end
 
   add_index "questions", ["user_id"], name: "index_questions_on_user_id"
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.datetime "created_at"
